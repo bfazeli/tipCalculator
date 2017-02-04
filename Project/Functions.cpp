911a04c90f7c83b249516e1058f4a0428c04fbd1
bijan102 <bfazeli1@gmail.com>
2017-02-03 23:24:14 -0800

    Declared and initi local variables if there were at least two nodes and checked to see if the list was odd, to bring the last node to the front of the list.

--- Project/Functions.cpp
+++ Project/Functions.cpp
@@ -15,3 +15,25 @@ func switchEveryTwo
 			Bring the last node to the front of the list and switch every other node (starting with the second node)
 			with the next node.
 */
+
+// Function definition
+void DoublyList::switchEveryTwo()
+{
+	if (count >= 2)
+	{
+		Node * temp = first;
+		int i = 0;
+
+		if (count % 2 != 0)		// Check to see if the list has odd elements, then bring the last node to the front
+		{
+			temp->setPreviousLink(last);
+			last->getPreviousLink()->setNextLink(nullptr);
+
+			last->setNextLink(first);
+			last = last->getPreviousLink();
+
+			temp->getPreviousLink()->setPreviousLink(nullptr);
+			first = temp->getPreviousLink();
+			i = 1;
+		}
+}
